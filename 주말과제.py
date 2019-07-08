65f0cfc5c9394d435b7b7c1178bddb80c2067886
KimDY90 <52019469+KimDY90@users.noreply.github.com>
2019-07-08 16:33:27 +0900

    Update 주말과제.py

--- 주말과제.py
+++ 주말과제.py
@@ -10,7 +10,7 @@ def my_split(str1, ch):
         h = len(b)
         d = a[ : c] + ' ' +a[(c+h) : ]
         e = [d]
-    break
+        break
     return e 
 
 
@@ -26,3 +26,19 @@ print(f)
 문장을 입력하세요: 아들아 너는 계획이 있구나
 단어를 입력하세요: 계획이
 ['아들아 너는   있구나']
+
+
+def my_max(number1, number2):##a와 b를 받을 때 동일한 틀에서 가능
+    
+    if a < b:
+      maximum = b
+    else:
+      maximum = a   
+
+return maximum
+
+
+d = my_max(a, b)
+a = int(input('숫자를 입력하세요: '))
+b = int(input('숫자를 입력하세요: ')) 
+print(d)
