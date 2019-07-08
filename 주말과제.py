a79beb01cb43bd3cc1b911589036dc645b21945e
KimDY90 <52019469+KimDY90@users.noreply.github.com>
2019-07-08 14:01:21 +0900

    Add files via upload

---
+++ 주말과제.py
@@ -0,0 +1,18 @@
+#주말 과제2
+def my_split(str1, ch):
+
+    if b in a:
+        c = a.index(b)
+        d = a[ : c] + '\n' +a[c : ]
+        e = list(d)
+    else:
+        print("error")
+    return e 
+
+
+a = input('문장을 입력하세요: ')
+b = input('단어를 입력하세요: ')
+f = my_split(a, b)
+
+
+print(f)
